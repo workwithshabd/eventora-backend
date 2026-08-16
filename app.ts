eae9cf5258fd63d3ee9c28c6b063ac1f40cff664
Shabd <workwithshabd@gmail.com>
2026-08-16 14:16:02 +0530

    t

--- app.ts
+++ app.ts
@@ -8,17 +8,15 @@ import bookingRoutes from "./routes/bookings.js";
 
 const app = express();
 
+const allowedOrigin =
+  process.env.CLIENT_URL || "http://localhost:5173";
 
-
-app.use(cors({
-
-  origin: "https://eventora-ruddy.vercel.app",
-
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-
-  allowedHeaders: ["Content-Type", "Authorization"],
-
-}));
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
